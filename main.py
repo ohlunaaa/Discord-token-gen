710892d40a522f73eaaed2aa17217e33abae02df
xharky <90693180+xharky@users.noreply.github.com>
2022-02-25 02:03:56 +0100

    Update main.py
    Xd

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def gen():
     proxy = random.choice(open("proxies.txt","r").read().splitlines())
     with sync_playwright() as p:
         for browser_type in [p.chromium]:
-            browser = browser_type.launch(headless=False,proxy={"server": f'http://{proxy}'})
+            browser = browser_type.launch(headless=False)
             xd = browser.new_context()
             page = xd.new_page()
             try:
